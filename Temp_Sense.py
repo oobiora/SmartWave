d161900b91a05259cf234b7c6d3e12edfd211c55
Olisa Obiora <oobiora22@gmail.com>
2019-11-13 17:26:32 -0500

    GOD PLS WORK
    ohh boyyy

--- Temp_Sense.py
+++ Temp_Sense.py
@@ -6,25 +6,37 @@ import adafruit_amg88xx
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
-def main(): #Declare main class
+def startHeating(foodTemp): #Declare main class
+
+    ##ALEXA STUFF##
+
+    ###############
+
+
     while not isHeated():  #Run Thermal Camera until correct temperature is reached
         for row in range(2,5):
             for x in range(2,5):
                 temp = amg.pixels[row][x]
                 print('{0:.1f}'.format((temp*1.8)+32), end=' || ')
             print()
-        print('\n',isHeated(), avgTemp(),'\n')
+        print('\n',isHeated(foodTemp), avgTemp(),'\n')
         time.sleep(1)
-    if isHeated():
+    if isHeated(foodTemp):
         print("The temperature has been reached")
 
-def isHeated():
+
+        ###ALEXASTUFF####
+
+
+        #################
+
+def isHeated(foodTemp):
     acc = 0
     for row in range(2,5):
         for x in range(2,5):
             temp = amg.pixels[row][x]
             acc += (temp*1.8)+32
-    if acc/9 >= 145.0:
+    if acc/9 >= foodTemp:
         return True
     else:
         return False
@@ -36,4 +48,4 @@ def avgTemp():
             temp = amg.pixels[row][x] 
             acc += (temp*1.8)+32
     return acc/9
-main()
+
