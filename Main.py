c67d1cec1e0a8ba0471f58c33048878117362d11
oobiora <oobiora22@gmail.com>
2019-11-13 16:52:09 -0500

    ughhhhh

--- Main.py
+++ Main.py
@@ -9,28 +9,29 @@ import os
 class SmartWave:
 
     
+    foodName = self.GrabData()
 ###Broken###
    # def openInputFile(): #precalled
        # while not os.path.exists(foodtemps.csv):
        #     print("File does not exist!")
        # return open(foodtemps.csv, "r")
-
-    def GrabData(): #precalled
+    @classmethod
+    def GrabData(self,): #precalled
         unorganizedData = DatacquireQRData()
         orgData = organizeData(unorganizedData)
-        return food
-
-    def organizeData(self,uoData): #precalled
+        return food, self
+    @classmethod
+    def organizeData(cls,uoData): #precalled
         orgData = []
         sData = uoData.split(',')
-        return sData[0]
-    
+        return sData[0], cls
+    @classmethod
     def acquireTemp():
         inFile = open('foodtemps.csv','r')
         translateDict = {}
         for line in inFile:
             translateDict[line[0]]=line[1]
-        return translateDict.get(self.GrabData())
+        return translateDict.get(cls.foodName)
     
     translatedFoodTemp = acquireTemp()
 
