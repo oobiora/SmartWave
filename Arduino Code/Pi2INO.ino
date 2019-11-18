bb9b03afedf471cef3b70466ad220239a23f6374
Olisa Obiora <oobiora22@gmail.com>
2019-11-18 16:01:10 -0500

    Update Pi2INO.ino
    servo comments

--- Arduino Code/Pi2INO.ino
+++ Arduino Code/Pi2INO.ino
@@ -1,22 +1,21 @@
-#include <Servo.h>
+#include <Servo.h> //include Servo library
+Servo myServo; // create servo object
 
-Servo myServo;
 
-
-void setup() {
-  Serial.begin(9600); 
-  myServo.attach(9);
+void setup() { //setup
+  Serial.begin(9600);  //activate Serial Monitor
+  myServo.attach(9); //attach servo to pin 9
   
  
   
 }
 
-void loop()
+void loop() //loop section
 {
   
   
   char val = Serial.read();
-      if (val == '1') {
+      if (val == '1') { // moves servo to the left if 1 is called. Stops after being turned on
          Serial.write("Left\n");
          for(int i = 0; i < 90; i++){
          myServo.write(5);
@@ -26,13 +25,13 @@ void loop()
       }
       else if(val == '0'){
          Serial.write("Right\n");
-         for(int i = 0; i < 90; i++){
+         for(int i = 0; i < 90; i++){ // moves servo to the right stops after being turned off
          myServo.write(180);
          delay(15);
          }
       } 
       else{
-         myServo.write(90);
+         myServo.write(90); // turns the servo off if nothing is immediatly called
       }
     
 
